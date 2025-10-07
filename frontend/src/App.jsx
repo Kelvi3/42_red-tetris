2fa29569de1fc8a5d0d03ca8625319036bb60144
Lucas <serbu.lucas@gmail.com>
2025-10-07 11:28:54 +0200

    Started project, init react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Board from './components/Game/Board'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Red Tetris</h1>
+      <Board />
+    </div>
+  )
+}
+
+export default App
