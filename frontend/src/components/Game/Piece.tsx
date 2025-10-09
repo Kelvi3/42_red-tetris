5d7a0ab6b269843ece45949d3dfad98842e75cc2
Lucas <serbu.lucas@gmail.com>
2025-10-09 13:40:23 +0200

    testing puzzle piece

---
+++ frontend/src/components/Game/Piece.tsx
@@ -0,0 +1,33 @@
+// types.ts
+type Piece = {
+  shape: number[][];
+  color: string;
+};
+
+// pieces.ts
+export const PIECES: { [key: string]: Piece } = {
+  I: {
+    shape: [
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ],
+    color: '#A020F0'
+  },
+  O: {
+    shape: [
+      [1, 1],
+      [1, 1]
+    ],
+    color: '#FFD700'
+  },
+  T: {
+    shape: [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]
+    ],
+    color: '#800080'
+  }
+};
