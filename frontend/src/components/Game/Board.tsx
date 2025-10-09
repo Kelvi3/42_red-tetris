04b8b4ac1f8da81d8ef3eda6c510356d45431b73
Lucas <serbu.lucas@gmail.com>
2025-10-09 11:34:55 +0200

    clean home done, navigate page, back button, ui started

--- frontend/src/components/Game/Board.tsx
+++ frontend/src/components/Game/Board.tsx
@@ -1,20 +1,34 @@
 // Board.tsx
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Board.css';
+// import { useEffect } from 'react';
 
 function Board() {
+  const location = useLocation();
+  const name = location.state.playerName || 'Player1';
+
   const ROWS = 20;
   const COLS = 10;
-  const emptyBoard = Array(ROWS).fill(null).map(() => Array(COLS).fill(0));
+  const emptyBoard = Array(ROWS)
+    .fill(null)
+    .map(() => Array(COLS).fill(0));
 
   return (
-    <div className="board">
-      {emptyBoard.map((row, rowIndex) => (
-        <div key={rowIndex} className="row">
-          {row.map((cell, colIndex) => (
-            <div key={colIndex} className="cell" />
-          ))}
-        </div>
-      ))}
+    <div>
+      <Link to="/">
+        <button className="game-button">Back</button>
+        </Link>
+      <h1>{name}</h1>
+      <div className="board">
+        {emptyBoard.map((row, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {row.map((cell, colIndex) => (
+              <div key={colIndex} className="cell" />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
